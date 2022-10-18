7e26bf3224e13b08966af7e5a0daeb3397315bb4
samuelsteven0902 <samuelsteven@student.uns.ac.id>
2022-10-18 09:23:39 +0700

    menambahkan beberapa CRUD controller

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\MuseumController;
 use App\Http\Controllers\API\KategoriController;
 use App\Http\Controllers\API\PengunjungController;
+use App\Http\Controllers\API\FAQController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,8 +25,17 @@ Route::get('/pengunjung',[PengunjungController::class, 'show']);
 
 Route::get('/show_museum',[MuseumController::class, 'show']);
 Route::get('/show_category/{museumId}',[KategoriController::class, 'show']);
-Route::get('/show_harga/{id_category}',[HargaController::class, 'show']);
-// Route::post('/show_data/{id_category}',[PengunjungController::class, 'show_data']);
+Route::post('/show_data/{id_category}',[PengunjungController::class, 'show_data']);
+Route::get('/show_harga',[HargaController::class, 'show']);
+Route::get('/edit-harga/{id_category}',[HargaController::class, 'edit_show']);
+Route::put('/update-harga/{id_category}',[HargaController::class, 'update']);
+Route::put('/hapus-harga/{id_category}',[HargaController::class, 'destroy']);
+
+Route::get('/show_faq',[FAQController::class, 'show']);
+Route::get('/edit_faq/{id_faq}',[FAQController::class, 'edit_show']);
+Route::put('/update_faq/{id_faq}',[FAQController::class, 'update']);
+Route::post('/add_faq',[FAQController::class, 'store']);
+Route::delete('/delete_faq/{id_faq}',[FAQController::class, 'destroy']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
